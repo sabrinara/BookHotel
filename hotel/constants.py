9a026a9ebfbe58cd18e2f79baf040cfbfbf2af38
sabrinara <saashid18@gmail.com>
2024-02-04 23:21:01 +0600

    booking model done

---
+++ hotel/constants.py
@@ -0,0 +1,9 @@
+BOOKING_STATUS = [
+    ('Completed', 'Completed'),
+    ('Pending', 'Pending'),
+    ('Ongoing', 'Ongoing'),
+]
+BOOKING_TYPES = [
+    ('Offline', 'Offline'),
+    ('Online', 'Online'),
+]
